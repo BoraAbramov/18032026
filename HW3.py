f42d8b184102dc6a551db4d7d8673ccf6d72f34c
BoraAbramov <borya812@gmail.com>
2026-03-26 23:48:50 +0200

    HW3 almost done

--- HW3.py
+++ HW3.py
@@ -11,9 +11,9 @@ Rules:
 
 Each spin shows 3 random symbols V
 The player starts with 50 money V
-Before each spin, ask the user how much they want to bet
-The bet must be between 1 and the current money
-The user can choose to quit the game
+Before each spin, ask the user how much they want to bet V
+The bet must be between 1 and the current money V
+The user can choose to quit the game V
 Update the player’s money after each round
 Winning rules:
 
@@ -46,7 +46,16 @@ Good luck'''
 
 
 import random
+
 def player_bet(money):
+    while True:
+        coins = int(input("How much are you betting"))
+        if coins > money:
+            print("You have not enough money")
+            continue
+        elif money >= coins >= 1:
+            money -= coins
+            return money
 
 def spinning_symbol(symbols) -> list:
     machine_screen = list()
@@ -55,17 +64,38 @@ def spinning_symbol(symbols) -> list:
         machine_screen.append(_symbol_in_machine)
     return machine_screen
 
+def calculation(bet, rate, machine_screen, symbols):
+    _option = [machine_screen[0], machine_screen[1], machine_screen[2]]
+
+    if _option[0] == _option[1] == _option[2]:
+        _factor = rate.index[symbols.index(machine_screen[0])] * bet * 777
+        return _factor
+    elif _option[0] == _option[1] or _option[0] == _option[2]:
+        _factor = rate.index[symbols.index(machine_screen[0])] * bet
+        return _factor
+    elif _option[1] == _option[2]:
+        _factor = rate.index[symbols.index(machine_screen[1])] * bet
+        return _factor
+    else:
+        return -bet
+
 
 rate    = [   2,     3,    9,     7,   11]
 symbols = ["🍒", "🍋", "⭐", "🔔", "💎"]
 money = 50
 print("=== SLOT MACHINE === \n")
 
-
 while money > 0:
-    bet = player_bet(money)
-    machine_screen = spinning_symbol(symbols)
-    print(machine_screen)
+    game = int(input("for game press 1, for quit press 2"))
+    if game == 1:
+        bet = player_bet(money)
+        machine_screen = spinning_symbol(symbols)
+        print(machine_screen)
+        money += calculation(bet, rate, machine_screen, symbols)
+        if money > 0:
+            print(f"You have {money}")
+    elif game == 2:
+        break
 
 print("=== GAME OVER === \n" f"you have {money} money left")
 
